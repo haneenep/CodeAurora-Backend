49f2ae4dc5d0c60935efe5f5c3fb548bab3d049e
Muhammed Haneen <haneenep134@gmail.com>
2025-02-16 20:44:42 +0530

    fix(auth-service): refresh token and access token

--- auth-service/src/lib/common/middlewares/jwtMiddleware.ts
+++ auth-service/src/lib/common/middlewares/jwtMiddleware.ts
@@ -1,29 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-import jwt, { TokenExpiredError, JsonWebTokenError } from "jsonwebtoken";
 import { config } from "dotenv";
-import { generateAccessToken } from "../../../utils/token";
+import { generateAccessToken, verifyToken } from "../../../utils/token";
 import { ERROR_MESSAGES } from "../../../constants/ErrorResponses";
 import { UserPayload } from "../../../types/authTypes";
 
 config();
-
-
-const verifyToken = (token: string, secret: string) => {
-  console.log(token, secret, "kk");
-
-  try {
-    return jwt.verify(token, secret) as UserPayload;
-  } catch (error) {
-    if (
-      error instanceof TokenExpiredError ||
-      error instanceof JsonWebTokenError
-    ) {
-      console.error("Error verifying token", error.message);
-      return null;
-    }
-    throw error;
-  }
-};
 
 export const jwtMiddleWare = async (
   req: Request,
@@ -35,52 +16,49 @@ export const jwtMiddleWare = async (
 
     let user: UserPayload | null = null;
 
+    // Verify access token
     if (access_token) {
-      user = verifyToken(
-        access_token,
-        process.env.ACCESS_TOKEN_SECRET as string
-      );
-
-      console.log(user);
+      console.log("access token:", access_token);
+      user = await verifyToken(access_token, process.env.ACCESS_TOKEN_SECRET!);
+      console.log("User from accesstkn", user);
     }
 
+    // If access token is invalid or expired, try refresh token
     if (!user && refresh_token) {
-      user = verifyToken(
-        refresh_token,
-        process.env.REFRESH_TOKEN_SECRET as string
-      );
-
-      console.log(user, "refreh token");
+      console.log("refresh token fnd", refresh_token);
+      user = await verifyToken(refresh_token, process.env.REFRESH_TOKEN_SECRET!);
+      console.log("User from refreshtkn", user);
 
       if (user) {
+        // Generate a new access token
         const newAccessToken = generateAccessToken({
           _id: user._id,
           email: user.email,
           role: user.role,
         });
 
+        // Set the new access token in cookies
         res.cookie("access_token", newAccessToken, {
           httpOnly: true,
           secure: true,
           sameSite: "none",
         });
+        console.log("New access token generated:", newAccessToken);
       }
     }
 
+    // If no user is found, return 401
     if (!user) {
+      console.log("User is unauthorized");
       res.status(401).json({ message: ERROR_MESSAGES.UNAUTHORIZED });
       return;
     }
 
-    console.log(user, "user after set the jwt");
-
+    console.log("User authenticated successfully:", user);
     req.user = user;
     next();
   } catch (error) {
-    console.error("Error in jwt middlewear", error);
-    res
-      .status(500)
-      .json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
-      return;
+    console.error("Error in JWT middleware:", error);
+    res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
   }
 };
