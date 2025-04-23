6998b0cbaf125ec6b8132eba29dc10e87885b51f
Muhammed Haneen <haneenep134@gmail.com>
2025-04-23 15:28:52 +0530

    refactor(api-gateway): validate dotenv

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -2,16 +2,29 @@ import express, { Application } from "express";
 import cors from "cors";
 import proxy from "express-http-proxy";
 import { config } from "dotenv";
+import { validateEnv } from "./utils/validateEnv";
+import { env } from "./utils/configEnv";
 
 config();
+validateEnv()
 
 const app : Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const allowedOrigins = [
+  env.FRONTEND_URL
+]
+
 const corsOptions: cors.CorsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin: function(origin, callback) {
+    if(allowedOrigins.includes(origin as string)){
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by cors"));
+    }
+  },
   credentials: true
 }
 
@@ -20,14 +33,18 @@ app.use(cors(corsOptions))
 const routes = [
   {
     path: "/auth",
-    target: String(process.env.AUTH_SERVICE),
+    target: env.AUTH_SERVICE,
     changeOrigin: true
+  },
+  {
+    path: "/problem",
+    target: env.PROBLEM_SERVICE
   }
 ]
 
 routes.forEach((route) => app.use(route.path, proxy(route.target)));
 
-const PORT : number = Number(process.env.PORT);
+const PORT : number = Number(env.PORT);
 
 app.listen(PORT,() : void =>  {
   console.log(`api-gateway is running on http://localhost${PORT}`)
---
+++ api-gateway/src/utils/validateEnv.ts
@@ -0,0 +1,16 @@
+import { env } from "./configEnv";
+
+export function validateEnv() {
+    if(!env.PORT){
+        throw new Error("PORT is not provided in the env")
+    }
+    if(!env.FRONTEND_URL){
+        throw new Error("FRONTEND_URL is not provided in the env")
+    }
+    if(!env.AUTH_SERVICE){
+        throw new Error("AUTH is not provided in the env")
+    }
+    if(!env.PROBLEM_SERVICE){
+        throw new Error("PROBLEM is not provided in the env")
+    }
+}
